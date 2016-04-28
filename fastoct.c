4a1774760cc6b56b46ac3be9730f615cb2805762
Rolf Madsen <rolfrm@gmail.com>
2016-04-28 14:33:10 +0200

    Super trees

--- fastoct.c
+++ fastoct.c
@@ -1,16 +1,22 @@
 #include "common.h"
+
 typedef struct{
   u16 * data;
   size_t size;
-}octree;
+}octree16;
 
-void octree_delete(octree * oct){
+void octree16_delete(octree16 * oct){
   dealloc(oct->data);
   oct->data = NULL;
   oct->size = 0;
 }
 
-int calc_octree_size(octree oct){
+typedef struct{
+  octree16 * tree;
+  int offset;
+}octree16_node;
+
+int calc_octree16_size(octree16 oct){
   int items = 0;
   u16 header = oct.data[0];
   ASSERT(header <= 255);
@@ -21,7 +27,7 @@ int calc_octree_size(octree oct){
 
     if(st){
       oct.data += 1;
-      items += calc_octree_size(oct);
+      items += calc_octree16_size(oct);
       oct.data += data;
     }else{
       oct.data += 1;
@@ -60,9 +66,9 @@ typedef struct{
   u16 * data;
   int size;
   float min_size;
-}octree_distance_field;
+}octree16_distance_field;
 
-int octree_from_distance_field_it(ray * r, octree_distance_field * oct, int offset){
+int octree16_from_distance_field_it(ray * r, octree16_distance_field * oct, int offset){
   ASSERT(offset< oct->size);
   
   float d = oct->f(r, oct->fieldinfo);
@@ -87,7 +93,7 @@ int octree_from_distance_field_it(ray * r, octree_distance_field * oct, int offs
     vec3 v = vec3_scale(vec3_sub(vec3_new(i % 2, (i / 2) % 2, (i / 4) % 2), vec3_half), r->size);
     
     _r.position = vec3_add(r->position, v);
-    int subtree_size = octree_from_distance_field_it(&_r, oct, offset + o2);
+    int subtree_size = octree16_from_distance_field_it(&_r, oct, offset + o2);
 
     if(subtree_size > 1){
       header |= (1 << i);
@@ -106,8 +112,8 @@ int octree_from_distance_field_it(ray * r, octree_distance_field * oct, int offs
   return sub_size;
 }
 
-octree octree_from_distance_field(float (* f)(ray * r, void * fieldinfo), void * fieldinfo){
-  octree_distance_field odf = {
+octree16 octree16_from_distance_field(float (* f)(ray * r, void * fieldinfo), void * fieldinfo){
+  octree16_distance_field odf = {
     .f = f,
     .fieldinfo = fieldinfo,
     .data = alloc0(10 * sizeof(u16)),
@@ -119,14 +125,14 @@ octree octree_from_distance_field(float (* f)(ray * r, void * fieldinfo), void *
     .position = vec3_half, .direction = vec3_zero,
     .x = 0, .y = 0, .color = 0, .normal = vec3_zero, .size = 0.5};
   
-  octree_from_distance_field_it(&r0, &odf, 0);
+  octree16_from_distance_field_it(&r0, &odf, 0);
   
   memmove(odf.data, odf.data + 1, odf.size * sizeof(odf.data[0]));
   
-  return (octree){.data = odf.data, .size = odf.size};
+  return (octree16){.data = odf.data, .size = odf.size};
 }
 
-void trace_octree(octree oct, ray * r){
+void trace_octree16(octree16 oct, ray * r){
   u16 header = oct.data[0];
   vec3 mid = vec3_new1(0.5);
   vec3 side = vec3_zero;
@@ -166,11 +172,11 @@ void trace_octree(octree oct, ray * r){
       }
       
       if((1 << cell) & header){
-	octree noct = {.data = oct.data + offset + 1};
+	octree16 noct = {.data = oct.data + offset + 1};
 	vec3 position = vec3_sub(vec3_scale(p, 2.0f), cellv);
 	vec3 ppos = r->position;
 	r->position = position;
-	trace_octree(noct, r);
+	trace_octree16(noct, r);
 	
 	r->position = ppos;
       }else{
@@ -185,6 +191,18 @@ void trace_octree(octree oct, ray * r){
   }
 }
 
+struct _octree_ptr;
+typedef struct _octree_ptr octree_ptr;
+struct _octree_ptr{
+  u8 subs;
+  u8 compressed;
+  union{
+    octree_ptr * sub_node;
+    u16 color;
+    octree16 compressed_tree;
+  }nodes[8];
+};
+void trace_octree(octree_ptr oct, ray * r);
 bool fastoct_test(){
   int width = 512, height = 512;   
   u8 img[width * height];
@@ -201,7 +219,11 @@ bool fastoct_test(){
 			   cos(y * 20)*0.33 + 0.5),
       .radius = 0.2
     };
-    octree oct = octree_from_distance_field((void *) sphere_distance, &s);  
+    octree_ptr oct = {.compressed = 1 | 2};
+    oct.nodes[0].compressed_tree = octree16_from_distance_field((void *) sphere_distance, &s);
+    //oct.nodes[1].compressed_tree = octree16_from_distance_field((void *) sphere_distance, &s);    
+    oct.nodes[1] = oct.nodes[0];
+    
   for(float y = -3.5; y < 3.5; y+= 0.005){
   
     logd("Y: %f\n", y);
@@ -230,7 +252,7 @@ bool fastoct_test(){
     }
     u64 t2 = timestamp();
     logd("Rendering took %fs\n", ((float)(t2 - t1)) * 1e-6);
-    logd("octree size: %iB\n", oct.size * sizeof(u16));
+    //logd("octree16 size: %iB\n", oct.size * sizeof(u16));
 
 
     
@@ -279,8 +301,79 @@ bool fastoct_test(){
 	
       }
     }
-    //octree_delete(&oct);
+    //octree16_delete(&oct);
     
   }
   return TEST_SUCCESS;
+}
+
+
+
+int calc_octree_size(octree_ptr oct){
+  int subsize = 0;
+  for(int i = 0; i < 8; i++){
+    int mask = 1 << i;
+    if(mask & oct.subs)
+      subsize += calc_octree_size(*oct.nodes[i].sub_node);
+    else if(mask & oct.compressed)
+      subsize += calc_octree16_size(oct.nodes[i].compressed_tree);
+    else if(oct.nodes[i].color)
+      subsize += 1;
+  }
+  return subsize;
+}
+
+void trace_octree(octree_ptr oct, ray * r){
+  vec3 mid = vec3_new1(0.5);
+  vec3 side = vec3_zero;
+  vec3 midd = vec3_sub(mid, r->position);
+  vec3 sided = vec3_sub(side, r->position);
+  vec3 side_o = vec3_div(sided, r->direction);
+  vec3 midd_o = vec3_div(midd, r->direction);
+  float ds[] = {midd_o.x, midd_o.y, midd_o.z, side_o.x, side_o.y, side_o.z};
+  f32 ds3[] = {0, f32_infinity, f32_infinity, f32_infinity, f32_infinity, f32_infinity};
+  for(u32 i = 0; i < array_count(ds); i++){
+    f32 v = ds[i];
+    if(v < 0 || v > f32_sqrt3)
+      continue;
+    if(v < ds3[array_count(ds3) - 1])
+      ds3[array_count(ds3) - 1] = v;
+    
+    for(int i = array_count(ds3) - 1; i > 0; i--){
+      if(ds3[i] < ds3[i - 1])
+	SWAP(ds3[i], ds3[i - 1]);
+    }
+  }
+  for(u32 i = 0; i < array_count(ds3); i++){
+    float d = ds3[i];
+    if(d < f32_infinity){
+      vec3 p = vec3_add(r->position, vec3_scale(r->direction, d + 0.00001));
+      if(p.x < 0.0 || p.y < 0.0 || p.z < 0.0 || p.x > 1.0 || p.y > 1.0 || p.z > 1.0)
+	continue;
+      vec3 cellv = vec3_gteq(p, vec3_half);
+
+      int cell = (p.x > 0.5 ? 1 : 0) + (p.y > 0.5 ? 2 : 0) + (p.z > 0.5 ? 4 : 0) ;
+      int mask = 1 << cell;
+      if(false == ((oct.compressed | oct.subs) & mask)){
+	u16 color = oct.nodes[cell].color;
+	r->color = color;
+	r->color_ptr = &oct.nodes[cell].color;
+	
+      }else{
+	vec3 position = vec3_sub(vec3_scale(p, 2.0f), cellv);
+	vec3 ppos = r->position;
+	r->position = position;
+      
+	if(oct.compressed & mask){
+	  trace_octree16(oct.nodes[cell].compressed_tree, r);
+	}else{
+	  trace_octree(*oct.nodes[cell].sub_node, r);
+	}
+	r->position = ppos;
+      }
+      if(r->color)
+	return;
+    }
+    else return;
+  }
 }
